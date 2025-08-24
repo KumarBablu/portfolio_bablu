1e006ead7a3e15e67327502a76a64122f0b13e07
Bablu Kumar <bablusoni2825@gmail.com>
2025-08-24 15:55:36 +0530

    First commit - portfolio setup

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/portfolio_bablu/", // 
+  base: "/portfolio_bablu/", // 👈 must match repo name
   plugins: [react()],
 });
